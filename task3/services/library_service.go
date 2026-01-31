7a7cb84f94b45cca51545754f8b97ea0058c3cef
Rajaf Dereje <rajafgudeta@gmail.com>
2026-01-31 16:19:15 +0300

    fix: update return type in LibraryControllerInterface

--- task3/services/library_service.go
+++ task3/services/library_service.go
@@ -6,12 +6,12 @@ import (
 )
 
 type LibraryManager interface {
-	AddBook(book models.Book)
-	RemoveBook(bookID int)
+	AddBook(book models.Book) error
+	RemoveBook(bookID int) error
 	BorrowBook(bookID int, memberID int) error
 	ReturnBook(bookID int, memberID int) error
 	ListAvailableBooks() []models.Book
-	ListBorrowedBooks(memberID int) []models.Book
+	ListBorrowedBooks(memberID int) ([]models.Book, error)
 }
 
 type Library struct {
