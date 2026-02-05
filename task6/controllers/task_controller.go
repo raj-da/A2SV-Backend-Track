e50d4aa2e8ce356da1421f4e0f881b5cd96f7a36
Rajaf Dereje <rajafgudeta@gmail.com>
2026-02-05 19:18:26 +0300

    add controllers

---
+++ task6/controllers/task_controller.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"net/http"
+	"task-manager/data"
+	"task-manager/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetTasks(c *gin.Context) {
+	tasks, err := data.GetAllTasks()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch tasks"})
+		return
+	}
+	c.JSON(http.StatusOK, tasks)
+}
+
+func GetTask(c *gin.Context) {
+	id := c.Param("id")
+	task, err := data.GetTaskByID(id)
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func CreateTask (c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createTask, err := data.CreateTask(task)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create task"})
+		return
+	}
+	c.JSON(http.StatusCreated, createTask)
+}
+
+func UpdateTask(c *gin.Context) {
+	// TODO: test by ommiting a field for update 
+	id := c.Param("id")
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := data.UpdateTask(id, task); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Task updated"})
+}
+
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+	if err := data.DeleteTask(id); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Task deleted"})
+}
