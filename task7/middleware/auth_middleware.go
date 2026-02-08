974517d51b6c0cffd81b36e2d77b8af9ff4684de
Rajaf Dereje <rajafgudeta@gmail.com>
2026-02-08 16:51:13 +0300

    add middleware

---
+++ task7/middleware/auth_middleware.go
@@ -0,0 +1,80 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+	"task-manager/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var JwtSecret = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+type Claims struct {
+	
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+            c.JSON(http.StatusUnauthorized, gin.H{
+                "error": "Invalid authorization header format",
+            })
+            c.Abort()
+            return
+        }
+
+		tokenString := parts[1]
+		claims := &models.Claims{}
+
+		token, err := jwt.ParseWithClaims(
+			tokenString,
+			claims,
+			func(token *jwt.Token) (interface{}, error) {
+				// Validate signing algorithm
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, jwt.ErrSignatureInvalid
+				}
+				return JwtSecret, nil
+			},
+		)
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid or expired token",
+			})
+			c.Abort()
+			return
+		}
+
+		// Store claims in context
+		c.Set("username", claims.ID)
+		c.Set("role", claims.Role)
+
+		c.Next()
+		
+	}
+
+}
+
+func AdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, _ := c.Get("role")
+		if role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Admin access required"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
