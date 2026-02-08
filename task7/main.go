c4b2ba08b7f015af718ef34ec9388c14611b96cd
Rajaf Dereje <rajafgudeta@gmail.com>
2026-02-08 17:44:12 +0300

    add entry point

--- task7/main.go
+++ task7/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"task-manager/data"
+	"task-manager/router"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGODB_URL")))
+	if err != nil {
+		log.Fatal("Error: ", err.Error())
+	}
+
+	db := client.Database("task_manager_2")
+	data.TaskCollection = db.Collection("tasks")
+	data.UserCollection = db.Collection("users")
+
+	router := router.SetupRouter()
+	router.Run()
 }
