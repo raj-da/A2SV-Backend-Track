95939be7d6348f550ec2c5642f15d5a972e85dcf
Rajaf Dereje <rajafgudeta@gmail.com>
2026-02-11 22:38:47 +0300

    add task use case implementation

---
+++ task8/Usecases/task_usecase.go
@@ -0,0 +1,36 @@
+package usecases
+
+import (
+	"context"
+	domain "task-manager/Domain"
+)
+
+type taskUsercase struct {
+	taskRepo domain.TaskRepository
+}
+
+func NewTaskUsercase(repo domain.TaskRepository) domain.TaskUsecase {
+	return &taskUsercase{
+		taskRepo: repo,
+	}
+}
+
+func (tu *taskUsercase) Create(ctx context.Context, task domain.Task) error {
+	return tu.Create(ctx, task)
+}
+
+func (tu *taskUsercase) GetByID(ctx context.Context, id string) (domain.Task, error) {
+	return tu.GetByID(ctx, id)
+}
+
+func (tu *taskUsercase) GetAll(ctx context.Context) ([]domain.Task, error) {
+	return tu.GetAll(ctx)
+}
+
+func (tu *taskUsercase) Update(ctx context.Context, id string, task domain.Task) error {
+	return tu.taskRepo.Update(ctx, id, task)
+}
+
+func (tu *taskUsercase) Delete(ctx context.Context, id string) error {
+	return tu.taskRepo.Delete(ctx, id)
+}
