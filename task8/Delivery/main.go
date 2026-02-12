fc072d9fb02edde4eaf0260527768890a1bbf305
Rajaf Dereje <rajafgudeta@gmail.com>
2026-02-12 18:33:35 +0300

    setup main

--- task8/Delivery/main.go
+++ task8/Delivery/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"task-manager/Delivery/routers"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGODB_URL")))
+	if err != nil {
+		log.Fatal("Error: ", err.Error())
+	}
+
+	db := client.Database("task_manager_3")
+	router := routers.SetUp(db)
+
+	router.Run()
 }
--- task8/go.mod
+++ task8/go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver/v2 v2.5.0
 	golang.org/x/crypto v0.40.0
 )
