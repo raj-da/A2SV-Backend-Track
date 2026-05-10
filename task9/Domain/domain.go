eb0f5d680ffdcd76ee5992b526e0b49ec266c4e7
Rajaf Dereje <rajafgudeta@gmail.com>
2026-05-10 23:13:18 +0300

    add refresh token in domain layer

--- task9/Domain/domain.go
+++ task9/Domain/domain.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 //* --- --- --- ---//
 //*    Entites     //
 //* --- --- --- ---//
-// Task entity
+// --- Task entity ---
 type Task struct {
 	ID 			bson.ObjectID `json:"id" bson:"_id,omitempty"`
 	Title 		string        `json:"title" bson:"title"`
@@ -19,7 +20,7 @@ type Task struct {
 	Status 		string        `json:"status" bson:"status"`
 }
 
-// User entity
+// --- User entity ---
 type User struct {
 	ID 		 bson.ObjectID `json:"id" bson:"_id,omitempty"`
 	Username string        `json:"username" bson:"username" binding:"required"`
@@ -27,10 +28,24 @@ type User struct {
 	Role 	 string        `json:"role" bson:"role"`
 }
 
+// --- Refresh token entity ---
+type RefreshToken struct {
+	ID 			bson.ObjectID	 `bson:"_id,omitempty"`
+	UserId 		bson.ObjectID	 `bson:"user_id"`
+	Token 		string			 `bson:"token"`
+	ExpiresAt 	time.Time		 `bson:"expires_at"`
+}
+
+type TokenClaims struct {
+	Username string `json:"username"`
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
 //* --- --- --- --- --- ---//
 //*   Repository Interface //
 //* --- --- --- --- --- ---//
-// Task repository interface
+// --- Task repository interface ---
 type TaskRepository interface {
 	Create(ctx context.Context, task Task) error
 	GetByID(ctx context.Context, id string) (Task, error)
@@ -39,12 +54,22 @@ type TaskRepository interface {
 	Delete(ctx context.Context, id string) error
 }
 
-// User repository interface
+// --- User repository interface ---
 type UserRepository interface {
 	Create(ctx context.Context, user User) error
 	GetByUsername(ctx context.Context, username string) (User, error)
+	GetByID(ctx context.Context, UserID string) (User, error)
+	GetByObjectID(ctx context.Context, UserID bson.ObjectID) (User, error)
 	UpdateRole(ctx context.Context, username string, role string) error
 	Count(ctx context.Context) (int64, error)
+}
+
+// --- Refresh token repository interface ---
+type RefreshTokenRepository interface {
+	Create(ctx context.Context, token RefreshToken) error
+	GetByToken(ctx context.Context, token string) (RefreshToken, error)
+	DeleteByToken(ctx context.Context, token string) error
+	DeleteByUserID(ctx context.Context, userID string) error
 }
 
 //* --- --- --- --- --- ---//
@@ -62,6 +87,20 @@ type TaskUsecase interface {
 // User usecase interface
 type UserUsecase interface {
 	Register(ctx context.Context, user User) error
-	Login(ctx context.Context, username, password string) (string, error)
+	Login(ctx context.Context, username, password string) (string, string, error)
 	PromoteUser(ctx context.Context, username string) error
+	Refresh(ctx context.Context, refreshToken string) (string, error)
+}
+
+//* --- --- --- --- --- --- ---//
+//*   Infrastructure Interface //
+//* --- --- --- --- --- --- ---//
+type JWTService interface {
+	GenerateAccessToken(user User) (string, error)
+	ValidateAccessToken(token string) (*TokenClaims, error)
+}
+
+type TokenService interface {
+	GenerateRandomString(length int) (string, error)
+	HashToken(token string) string
 }
