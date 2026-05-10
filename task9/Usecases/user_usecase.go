f5c3f762856f529750c27bcede57d4ed2006b4fc
Rajaf Dereje <rajafgudeta@gmail.com>
2026-05-10 23:14:26 +0300

    add refresh token to user usecase

--- task9/Usecases/user_usecase.go
+++ task9/Usecases/user_usecase.go
@@ -3,20 +3,40 @@ package usecases
 import (
 	"context"
 	"errors"
+	"fmt"
 	domain "task-manager/Domain"
 	infrastructure "task-manager/Infrastructure"
+	"time"
 )
 
 
 type userUsecase struct {
 	userRepo domain.UserRepository
+	refreshTokenRepo domain.RefreshTokenRepository
+	jwtService domain.JWTService
+	tokenService domain.TokenService
 }
 
-func NewUserUsecase (repo domain.UserRepository) domain.UserUsecase {
-	return &userUsecase{userRepo: repo}
+func NewUserUsecase (
+	userRepo domain.UserRepository, 
+	refresthTokenRepo domain.RefreshTokenRepository, 
+	jwtSvc domain.JWTService,
+	tokenSvc domain.TokenService,
+	) domain.UserUsecase {
+	return &userUsecase{
+		userRepo: userRepo,
+		refreshTokenRepo: refresthTokenRepo,
+		jwtService: jwtSvc,
+		tokenService: tokenSvc,
+	}
 }
 
 func (uu *userUsecase) Register(ctx context.Context, user domain.User) error {
+	// Check if username already exists
+	_, err := uu.userRepo.GetByUsername(ctx, user.Username)
+	if err == nil {
+		return fmt.Errorf("Username already exists")
+	}
 	// Login: First user is admin
 	count, err := uu.userRepo.Count(ctx)
 	if err != nil {
@@ -34,24 +54,61 @@ func (uu *userUsecase) Register(ctx context.Context, user domain.User) error {
 	return uu.userRepo.Create(ctx, user)
 }
 
-func (uu *userUsecase) Login(ctx context.Context, username, password string) (string, error) {
+func (uu *userUsecase) Login(ctx context.Context, username, password string) (string, string,error) {
 	// Find user
 	var user domain.User
-	user, err := uu.userRepo.GetByUsername(ctx, username)
-	if err != nil {
-		return "", err
+	user, usrErr := uu.userRepo.GetByUsername(ctx, username)
+	if usrErr != nil {
+		return "", "", usrErr
 	}
 
 	// Match password
 	if match := infrastructure.ComparePassword(password, user.Password); !match {
-		return "", errors.New("Incorrect cridential")
+		return "", "", errors.New("Incorrect cridential")
 	}
 
 	// Generate token
-	token, err := infrastructure.GenerateToken(&user)
-	return token, err
+	accessToken, _ := uu.jwtService.GenerateAccessToken(user)
+
+	// Generate Refresh token (Opaque token/string)
+	rawRefreshToken, _ := uu.tokenService.GenerateRandomString(32)
+
+	// Hash
+	hashedToken := uu.tokenService.HashToken(rawRefreshToken)
+	
+	// Store refresh token in DB
+	err := uu.refreshTokenRepo.Create(ctx, domain.RefreshToken{
+		UserId: user.ID,
+		Token: hashedToken,
+		ExpiresAt: time.Now().Add(time.Hour * 24 * 7),
+	})
+
+	return accessToken, rawRefreshToken, err
 }
 
 func (uu *userUsecase) PromoteUser(ctx context.Context, username string) error {
 	return uu.userRepo.UpdateRole(ctx, username, "admin")
+}
+
+func (uu *userUsecase) Refresh(ctx context.Context, rawToken string) (string, error) {
+	// hash incoming token
+	hashedToken := uu.tokenService.HashToken(rawToken)
+
+	// check db
+	storedToken, err := uu.refreshTokenRepo.GetByToken(ctx, hashedToken)
+	if err != nil {
+		return "", errors.New("invalid refresh token")
+	}
+
+	// check expiry
+	if time.Now().After(storedToken.ExpiresAt) {
+		uu.refreshTokenRepo.DeleteByToken(ctx, hashedToken)
+		return "", errors.New("refresh token expired")
+	}
+
+	// generate new access token
+	user, _ := uu.userRepo.GetByObjectID(ctx, storedToken.UserId)
+	newAccessToken, _ := uu.jwtService.GenerateAccessToken(user)
+
+	return newAccessToken, nil
 }
